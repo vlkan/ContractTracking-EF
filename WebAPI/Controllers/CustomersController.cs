db8da1f2d4ffd5738dc331d1c9760b584e6daafa
vlkan <thkvolkanonder@gmail.com>
2022-07-21 15:18:56 +0300

    customer controller test

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    [HttpGet]
+    public List<Customer> Get() 
+    {
+        return new List<Customer>();
+    }
+}
